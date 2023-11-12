7afddcb472d569d413f3af1728d75df7e973f8a4
Haaris Iqubal <mdhaarisiqubal@icloud.com>
2023-11-12 12:13:53 +0100

    Added test cases

---
+++ math_quiz/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+def test_function_C(self):
+
+    #         test_cases = [
+
+    #             (5, 2, '+', '5 + 2', 7),
+
+    #             ''' TODO add more test cases here '''
+
+    #         ]
+
+
+    #         for num1, num2, operator, expected_problem, expected_answer in test_cases:
+
+    #             # TODO
+
+    #             pass
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -7,7 +7,7 @@ from math_quiz import input_number, choose_sign, apply_operation
 class TestMathGame(unittest.TestCase):
 
 
-    def test_function_A(self):
+    def test_input_number(self):
 
         # Test if random numbers generated are within the specified range
 
@@ -21,29 +21,32 @@ class TestMathGame(unittest.TestCase):
 
             self.assertTrue(min_val <= rand_num <= max_val)
 
-
-    def test_function_B(self):
-
+    def test_choose_sign(self):
         # TODO
+        # Test if the sign generated is valid from sign array or not
+        valid_signs = ['+', '-', '*']
+        for _ in range(len(valid_signs)):
+            result = choose_sign()
+            self.assertIn(result, valid_signs, f"The result '{result}' is not a valid mathematical sign")
+            pass
 
-        pass
-
-
-    def test_function_C(self):
+    def test_apply_operation(self):
 
             test_cases = [
-
                 (5, 2, '+', '5 + 2', 7),
-
-                ''' TODO add more test cases here '''
-
+                (12, 5, '*', '12 * 5', 60),
+                (5, 7, '-', '5 - 7', -2),
+                (8, 8, '+', '8 + 8', 16),
+                (9, 4, '-', '9 - 4', 5)
             ]
 
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
 
                 # TODO
-
+                PROBLEM, ANSWER = apply_operation(num1,num2,operator)
+                self.assertEqual(expected_problem,PROBLEM, f"The expected problem don't match the problem. Expected Problem: {expected_problem}, Problem: {PROBLEM}")
+                self.assertEqual(expected_answer,ANSWER, f"The expected problem don't match the problem. Expected Answer: {expected_answer}, Answer: {ANSWER}")
                 pass
 
 
