44fcbb37973225107a69a74c64354cdf007f101d
Haaris Iqubal <mdhaarisiqubal@icloud.com>
2023-11-12 12:51:11 +0100

    Change in setup file

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+with open('./README.md','r') as f:
+    long_description = f.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Math Quiz Package'
+LONG_DESCRIPTION = 'This is math quiz package where user enter input and this app will return output and check user answer and own answer and assign scores'
+
+# Setting up
+setup(
+    name="mathquiz",
+    version=VERSION,
+    author="Haaris (Haaris Iqubal)",
+    author_email="<haaris.iqubal@fau.de>",
+    license="Apache",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/HaarisIqubal/dsss_homework_2/',
+    packages=find_packages(where='math_quiz'),
+    keywords=['math quiz'],
+    classifiers=[
+        "Development Status :: 1 - Final",
+        "Intended Audience :: User",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.8.7",
+)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
